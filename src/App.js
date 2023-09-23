952bbdcc658f2f2528cff0c77b1b4f02646df24c
Shimmybaks <133874988+Shimmybaks@users.noreply.github.com>
2023-09-23 19:28:26 +0200

    Added Github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>This project was coded by Shimei Bakala and is {""}
+        <a
+          href="https://github.com/Shimmybaks/react-weather-app"
+          target="_blank" rel="noreferrer"
+        >open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
