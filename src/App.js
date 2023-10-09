00ae9297ad84fd84c9d15ffdd9c825252581bd97
Shimmybaks <133874988+Shimmybaks@users.noreply.github.com>
2023-10-10 01:06:31 +0200

    Edit footer

--- src/App.js
+++ src/App.js
@@ -9,30 +9,32 @@ function App() {
         <Weather defaultCity="Kinshasa" />
 
         <footer>
-          This project was coded by{" "}
-          <a
-            href="https://64df9c563519d60008d85a39--fancy-kringle-0f2164.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Shimei Bakala
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Shimmybaks/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://650f3f4e4f9d260008fc9bb0--iridescent-figolla-4087b5.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            hosted by Netlify
-          </a>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://64df9c563519d60008d85a39--fancy-kringle-0f2164.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Shimei Bakala
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/Shimmybaks/react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://650f3f4e4f9d260008fc9bb0--iridescent-figolla-4087b5.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted by Netlify
+            </a>
+          </p>
         </footer>
       </div>
     </div>
